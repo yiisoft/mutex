baea01495b1119cc33c7bd6747de2cdbe0b5711f
Andrii Vasyliev <sol@hiqdev.com>
2018-11-10 17:43:54 +0000

    Inited tests (not passing yet)

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+// ensure we get report on all possible php errors
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
