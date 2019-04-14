1e4a0c5ef27e21358e5103ad233bbfe0e5d76003
Alexander Makarov <sam@rmcreative.ru>
2019-04-14 18:06:12 +0300

    Apply fixes from StyleCI (#8)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @link http://www.yiiframework.com/
+ *
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
@@ -8,4 +9,4 @@
 // ensure we get report on all possible php errors
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
