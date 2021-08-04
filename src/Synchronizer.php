894d667995b75b5c88e318995b640df5a2a9c181
devanych <mail@devanych.ru>
2021-08-04 15:08:06 +0300

    Add docBlocks and fix psalm to Synchronizer

--- src/Synchronizer.php
+++ src/Synchronizer.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace Yiisoft\Mutex;
 
+use RuntimeException;
+
+/**
+ * Executes a callback in synchronized mode, i.e. only a single instance of the callback is executed at the same time:
+ *
+ * ```php
+ * $newCount = $synchronizer->execute('critical', function () {
+ *     return $counter->increase();
+ * }, 10);
+ * ```
+ */
 final class Synchronizer
 {
     private MutexFactoryInterface $mutexFactory;
@@ -13,12 +24,25 @@ final class Synchronizer
         $this->mutexFactory = $mutexFactory;
     }
 
+    /**
+     * Executes a PHP callable with a lock and returns the result.
+     *
+     * @param string $name Name of the mutex to acquire.
+     * @param callable $callback PHP callable to execution.
+     * @param int $timeout Time (in seconds) to wait for lock to be released. Defaults to zero meaning that
+     * method {@see MutexInterface::acquire()} will return false immediately in case lock was already acquired.
+     *
+     * @return mixed The result of the PHP callable execution.
+     */
     public function execute(string $name, callable $callback, int $timeout = 0)
     {
         $mutex = $this->mutexFactory->create($name);
+
         if (!$mutex->acquire($timeout)) {
-            throw new \RuntimeException("Unable to execute synchronized \"$name\".");
+            throw new RuntimeException("Unable to execute synchronized \"$name\".");
         }
+
+        /** @var mixed $result */
         $result = $callback();
         $mutex->release();
         return $result;
