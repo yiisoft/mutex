21b8f5719b94ef555bdcf9c687ffeee74d904c4a
Evgeniy Zyubin <mail@devanych.ru>
2022-01-12 12:27:10 +0300

    Fix #48 Throw an `InvalidArgumentException` if the retry delay is less than one millisecond

--- tests/RetryAcquireTraitTest.php
+++ tests/RetryAcquireTraitTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Mutex\Tests;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Mutex\Tests\Mocks\RetryAcquireTraitMutex;
 
@@ -33,6 +34,20 @@ final class RetryAcquireTraitTest extends TestCase
             ->withRetryDelay(1000);
 
         $this->assertFalse($mutex->acquire(1));
+    }
+
+    public function testRetryAcquireThrowExceptionWithNotPositiveRetryDelay(): void
+    {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            $this->markTestSkipped('usleep() is not reliable on Windows.');
+        }
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Retry delay value must be a positive number greater than zero, "0" is received.',
+        );
+
+        (new RetryAcquireTraitMutex(2))->withRetryDelay(0);
     }
 
     public function testImmutability(): void
